48c98b0a684aeb57b3c2ac3c6ec6ba9dc92b7c7f
Sharmi-v <sharmivictor12@gmail.com>
2025-03-12 14:48:51 +0530

    mnb

---
+++ concat.java
@@ -0,0 +1,9 @@
+public class concat
+{
+    public static void main(String args[])
+    {
+        String name1="Sharmi";
+        String name2="Victor";
+        System.out.println(name1.concat(name2));
+    }
+}
