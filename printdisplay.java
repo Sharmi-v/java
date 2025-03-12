a21bb4f3e4d283d2ae704eefd10379044472777c
Sharmi-v <sharmivictor12@gmail.com>
2025-03-12 14:42:51 +0530

    adg

--- printdisplay.java
+++ printdisplay.java
@@ -1,7 +1,7 @@
 public class printdisplay
 {
-    public static void main(String args[])
+    public static void main(String args[]) //main function
     {
-        System.out.println("hello world");
+        System.out.println("hello world");//to print result
     }
 }
---
+++ vari.java
@@ -0,0 +1,8 @@
+public class vari
+{
+    public static void main(String args[])
+    {
+        String name="sharmi";
+        System.out.println(name);
+    }
+}
