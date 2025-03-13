946825cf9a2ea2a3117b234eaa0cc54fb563e4f4
Sharmi-v <sharmivictor12@gmail.com>
2025-03-13 15:14:57 +0530

    condition

---
+++ nexted.java
@@ -0,0 +1,31 @@
+public class nexted
+{
+    public static void main(String args[])
+    {
+        String name="Sharmi.v";
+        String place="kanyakumari";
+        String subject="computer";
+        int mark=76;
+        if(mark>=90)
+        {
+            System.out.println("grade:O");
+        }
+        else if(mark>=80)
+        {
+            System.out.println("grade:A");
+        }
+        else if(mark>=70)
+        {
+            System.out.println("grade:B");
+        }
+        else if(mark>=60)
+        {
+            System.out.println("grade:C");
+        }
+        else
+        {
+            System.out.println("grade:f");
+
+        }
+    }     
+}
