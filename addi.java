306f4dfbc14e02edfb379430fa60502f58141c5a
Sharmi-v <sharmivictor12@gmail.com>
2025-03-13 11:07:07 +0530

    arith

---
+++ addi.java
@@ -0,0 +1,13 @@
+public class addi
+{
+    public static void main(String args[])
+    {
+        int a=6;
+        int b=8;
+        int c=a+b;
+        int d=a*b;
+        System.out.println(c);
+        System.out.println(d);
+        
+    }
+}
