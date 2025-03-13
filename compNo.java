771867c0a8d8a1e8ae672fde954fd3e479cb851a
Sharmi-v <sharmivictor12@gmail.com>
2025-03-13 12:09:04 +0530

    logi

---
+++ compNo.java
@@ -0,0 +1,19 @@
+public class compNo
+{
+    public static void main(String args[])
+    {
+        int a=94;
+        if(a==100 && a<90)
+        {
+            System.out.println("True");
+        }
+        if(a==90 || a<80)
+        {
+            System.out.println("True1");
+        }
+        else
+        {
+            System.out.println("invaild");
+        }
+    }
+}
