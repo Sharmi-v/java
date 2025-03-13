7c9628b6986bf4911eb7bb5f5b46c8d69106ae82
Sharmi-v <sharmivictor12@gmail.com>
2025-03-13 10:24:46 +0530

    gre

---
+++ grade.java
@@ -0,0 +1,18 @@
+public class grade
+{
+    public static void main(String args[])
+    {
+        String name="Sharmi.v";
+        String place="kanyakumari";
+        String subject="computer";
+        int mark=76;
+        char grade='B';
+        {
+            System.out.println(name);
+            System.out.println(place);
+            System.out.println(subject);
+            System.out.println(mark);
+            System.out.println(grade);
+        }
+    }
+}
