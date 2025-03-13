53556d33970fec41b0aeebe687353a7394a3f1c3
Sharmi-v <sharmivictor12@gmail.com>
2025-03-13 14:16:30 +0530

    len

---
+++ length.java
@@ -0,0 +1,9 @@
+public class length
+{
+    public static void main(String args[])
+    {
+        String a="a great book with many experience";
+        int b=a.length();
+        System.out.println("the lenth of the string is"+b);
+    }
+}
