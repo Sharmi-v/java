851fab3a79799342b52cbcc21d6bb117a37c9754
Sharmi-v <sharmivictor12@gmail.com>
2025-03-13 11:51:45 +0530

    compare

---
+++ comp.java
@@ -0,0 +1,17 @@
+public class comp
+{
+    public static void main(String args[])
+    {
+        String a="abcd";
+        String b="abcd";
+        if(a==b)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+           
+        }
+    }
+}
