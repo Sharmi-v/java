2a80def3c832093bc99e95eff10bdd3d24d7d6dc
Sharmi-v <sharmivictor12@gmail.com>
2025-03-14 13:10:24 +0530

    multiplication using method

---
+++ mulp.java
@@ -0,0 +1,14 @@
+public class mulp
+{
+   static void multi()
+    {
+        int a=5;
+        int b=6;
+        int c=a*b;
+        System.out.println(c);
+    }
+    public static void main(String args[])
+    {
+        multi();
+    }
+}
