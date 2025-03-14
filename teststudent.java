e54e058ef2e7a0a77838b80037d03f44c2c7e496
Sharmi-v <sharmivictor12@gmail.com>
2025-03-14 15:51:10 +0530

    class

---
+++ teststudent.java
@@ -0,0 +1,19 @@
+class student 
+ {
+    int id;
+    String name;
+ }
+    public class teststudent
+    {
+    public static void main(String args[])
+    {
+        student s1=new student();
+        s1.id=101;
+        System.out.println(s1.id);
+        student s2=new student();
+        s2.id=102;
+        System.out.println(s2.id);
+
+    }
+    }
+ 
