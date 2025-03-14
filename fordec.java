3c537b582e157d8caf344746a020efeb3827eafd
Sharmi-v <sharmivictor12@gmail.com>
2025-03-14 11:13:43 +0530

    for loop using decreament operator

---
+++ fordec.java
@@ -0,0 +1,10 @@
+public class fordec
+{
+    public static void main(String args[])
+    {
+        for(int i=10;i>=5;i--)
+        {
+            System.out.println("I am sharmi");
+        }
+    }
+}
