8ee0f7e1399be892d05c7e931471426ff207ec49
Sharmi-v <sharmivictor12@gmail.com>
2025-03-14 13:48:59 +0530

    array

---
+++ ar.java
@@ -0,0 +1,12 @@
+public class ar
+{
+    public static void main(String args[])
+    {
+        String[] name={"shuruthi","sharmi","sumi","shamee","subi"};
+        System.out.println("at index 0:"+name[0]);
+        System.out.println("at index 1:"+name[1]);
+        System.out.println("at index 2:"+name[2]);
+        System.out.println("at index 3:"+name[3]);
+    }
+
+}
