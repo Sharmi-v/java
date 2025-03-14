975ff64d7168253279d32212cb8e06eb7233b669
Sharmi-v <sharmivictor12@gmail.com>
2025-03-14 13:19:49 +0530

    using if statement in method

---
+++ bbb.java
@@ -0,0 +1,19 @@
+public class bbb
+{
+   static void vote()
+    {
+        int age=20;
+        if(age>=19)
+        {
+            System.out.println("eligible to vote");
+        }
+        else
+        {
+            System.out.println("not eligible to vote");
+        }
+    }
+    public static void main(String args[])
+    {
+        vote();
+    }
+}
