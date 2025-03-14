6e1c14f7a13879a38649ecf89b2b1dee24620a5f
Sharmi-v <sharmivictor12@gmail.com>
2025-03-14 10:51:31 +0530

    do while false

---
+++ falsedo.java
@@ -0,0 +1,13 @@
+public class falsedo
+{
+    public static void main(String args[])
+    {
+        int i=10;
+       do
+        {
+            System.out.println(10);
+            i++;    
+        }
+        while(i<=5);
+    }
+}
